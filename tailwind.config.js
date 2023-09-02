4b764d16e187736110078f40d3705cd0af1bb57f
Shivani-Sd <124508358+Shivani-Sd@users.noreply.github.com>
2023-09-02 10:38:55 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,30 +3,77 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     screens: {
-      'sm': {'max':'768px'},
-      'md': '768px',
-      'lg': '1104px',
-      'xl': '1535px',
-      '2xl': '1600px',
+      sm: { max: "768px" },
+      md: "768px",
+      lg: "1104px",
+      xl: "1535px",
+      "2xl": "1600px",
     },
     extend: {
+      animation: {
+        "slide-into-left": "slide-in-left 0.75s ease-in forwards",
+        "slide-into-left-max": "slide-in-left-max 0.75s ease-in forwards",
+        "slide-onto-left": "slide-out-right 0.75s ease-out backwards",
+        "slide-onto-left-max": "slide-out-right-max 0.75s ease-out backwards",
+      },
       keyframes: {
-        backgroundFade: {
-          "99.9%": {
-            background: "black",
+        "slide-in-left": {
+          "0%": {
+            transform: "translateX(-80%)",
+            opacity: "0",
           },
-          " 100%": {
-            background: "transparent",
+          "50%": {
+            opacity: "0.5",
+            transform: "translateX(-35%)",
+          },
+          "100%": {
+            transform: "translateX(20%)",
+            opacity: "1",
           },
         },
-        animation: {
-          backgroundFade: "backgroundFade 3s",
+        "slide-in-left-max": {
+          "0%": {
+            transform: "translateX(-180%)",
+            opacity: "0",
+            "50%": {
+              opacity: "0.5",
+              transform: "translateX(100%)",
+            },
+          },
+          "100%": {
+            transform: "translateX(180%)",
+            opacity: "1",
+          },
         },
-        fontFamily: {
-          netflix: ['Netflix Sans'],
-        }
+
+        "slide-out-right": {
+          "0%": {
+            transform: "translateX(20%)",
+            opacity: "1",
+            "50%": {
+              opacity: "0.5",
+            },
+          },
+
+          "100%": {
+            transform: "translateX(-80%)",
+            opacity: "0",
+          },
+        },
+        "slide-out-right-max": {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "0",
+          },
+        },
+      },
+      fontFamily: {
+        netflix: ["Netflix Sans"],
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animation-delay"),
+],
 };
